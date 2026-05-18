15b15a6a9c3e06ad65785d69adae51d1399ca15d
InDaviX <dszewczyk235@gmail.com>
2026-05-19 00:36:56 +0200

    a

---
+++ LostInSpace/src/Scripts/Platforms/OneTimePlatform.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class OneTimePlatform : Node3D, IPlatform
+{
+	public void OnEnter(TileContext context)
+	{
+	}
+
+	public void OnExit(TileContext context)
+	{
+	}
+}
