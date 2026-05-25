2275114629421d06fba37e1a2c6d738e37249013
Vetsou <virees@posteo.net>
2026-05-25 21:45:50 +0200

    Added platform resources and changed level

--- LostInSpace/src/Scripts/Platforms/BreakablePlatform.cs
+++ LostInSpace/src/Scripts/Platforms/BreakablePlatform.cs
@@ -3,13 +3,13 @@ using Godot;
 public class BreakablePlatform(int health = 1) : Platform
 {
 	private int Health { get; set; } = health;
-	public override PlatformVisualData VisualData { get; } = ResourceLoader.Load<PlatformVisualData>("uid://c7l8d1q81nlhq");
+	public override PlatformVisualData VisualData { get; } = ResourceLoader.Load<PlatformVisualData>("uid://cbne4iex327jv");
 
-    public override void OnExit(TileContext context)
-    {
+	public override void OnExit(TileContext context)
+	{
 		Health--;
 		if (Health == 0)
-        {
+		{
 			RemovePlatform();
 		}
 	}
--- LostInSpace/src/Scripts/Platforms/MovePlatform.cs
+++ LostInSpace/src/Scripts/Platforms/MovePlatform.cs
@@ -3,6 +3,6 @@ using Godot;
 public class MovePlatform(Vector2I direction) : Platform
 {
 	public Vector2I Direction { get; } = direction;
-	public override PlatformVisualData VisualData { get; } = ResourceLoader.Load<PlatformVisualData>("uid://c7l8d1q81nlhq");
+	public override PlatformVisualData VisualData { get; } = ResourceLoader.Load<PlatformVisualData>("uid://bt7fcql1et6em");
 	public override void OnEnter(TileContext context) => context.Player.Move(Direction);
 }
