d6d4012722631d5ff0fb0411e77425776c32892f
Vetsou <virees@posteo.net>
2026-06-13 17:52:13 +0200

    Added MainMenu tests

--- LostInSpace.Tests/Tests/UI/MainMenuTests.cs
+++ LostInSpace.Tests/Tests/UI/MainMenuTests.cs
@@ -1,5 +1,6 @@
 using GdUnit4;
 using GdUnit4.Constraints;
+using Godot;
 using LostInSpace.Scripts.UI;
 using LostInSpace.Scripts.UI.Scenes.MainMenu;
 using static GdUnit4.Assertions;
@@ -21,16 +22,24 @@ public class MainMenuTests
 				?? throw new InvalidOperationException("Could not cast scene");
     }
 
-	[TestCase(SceneId.LevelSelector)]
-    public async Task MainMenu_OnButtonClick_ShouldInvokeCorrectSignal(SceneId changeToSceneId)
+	[TestCase("PlayButton", SceneId.LevelSelector)]
+    [TestCase("CreateLevelButton", SceneId.CreateLevel)]
+    [TestCase("AchievementsButton", SceneId.Achievements)]
+    [TestCase("SettingsButton", SceneId.SettingsMenu)]
+    public async Task MainMenu_OnButtonClick_ShouldInvokeCorrectSignal(string btnName, SceneId sceneId)
     {
 		// Arrange
+        Button btn = runner.FindChild(btnName) as Button
+                ?? throw new InvalidOperationException("Could not find PlayButton");
 		ISignalConstraint monitor = AssertSignal(mainScene).StartMonitoring();
 
-        // Act
-        mainScene.OnMenuButtonPressed(changeToSceneId);
+		// Act
+		Vector2 pressPos = btn.GetGlobalRect().GetCenter();
+		runner.SetMousePos(pressPos);
+		runner.SimulateMouseButtonPressed(MouseButton.Left);
+		await runner.AwaitInputProcessed();
 
 		// Assert
-        await monitor.IsEmitted(MainMenu.SignalName.RequestSceneChange, 1).WithTimeout(100);
+		await monitor.IsEmitted(MainMenu.SignalName.RequestSceneChange, (int)sceneId).WithTimeout(100);
     }
 }
